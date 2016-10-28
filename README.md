df221caa9f3308e60cbf55fec35c77dfa98e8d93
anybalrocha <anybalrocha@hotmail.com>
2016-10-28 00:52:14 -0200

    Inclusão de projeto.

---
+++ README.md
@@ -0,0 +1,5 @@
+# ASP.NET MVC - Action Filters
+
+Projeto destinado aos desenvolvedores ASP.NET MVC, com o objetivo de apresentar os Action Filters, que são lógicas de filtragem que serão inseridas e programadas para executarem antes ou depois da chamada de um Action Method.
+
+Você insere no seu projeto para incluir um comportamento aos métodos da sua Controller, de forma a compor o comportamento padrão que seu Action Method já possui no escopo explicito.
---
+++ WebApplication1/Controllers/MeuFiltroAttribute.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1.Controllers
+{
+    public class MeuFiltroAttribute : ActionFilterAttribute
+    {
+        public string Roles { get; set; }
+        //public string Users { get; set; }
+
+        //ANTES de executar a Action
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //string[] userRoles = System.Web.Security.Roles.GetRolesForUser();
+            string[] userRoles = { "Admin", "Gerente" };
+
+            foreach (var controllerRole in this.Roles.Split(','))
+            {
+                foreach (var role in userRoles)
+                {
+                    if (controllerRole.Equals(role))
+                        return;
+                }
+            }
+
+            throw new SecurityException("Acesso negado.");
+
+
+
+
+            //var request = filterContext.HttpContext.Request;
+
+            //if (request.IsAjaxRequest())
+            //{
+            //    var response = filterContext.HttpContext.Response;
+            //    response.Write("IsAjaxRequest");
+            //}
+            //else
+            //    filterContext.Result = 
+            //        new RedirectToRouteResult(
+            //            new RouteValueDictionary(
+            //                new { controller = "Home", action = "Index" }));
+
+
+            base.OnActionExecuting(filterContext);
+        }
+
+
+        //DEPOIS de executar a Action
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
